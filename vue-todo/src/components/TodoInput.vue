3b413658b58e939a5d38f01703307fea09997c56
junwoo45 <sinyoorang@gmail.com>
2019-06-11 14:18:11 +0900

    methods 분리

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -17,6 +17,9 @@ export default {
   methods: {
     addTodo: function() {
       localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
       this.newTodoItem = "";
     }
   }
