60ff90d490a7a64076274e05d3e29e653c3bdf8b
junwoo45 <sinyoorang@gmail.com>
2019-06-11 14:36:06 +0900

    enter눌렀을때도 아이템등록&CSS적용

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem">
-    <button @click="addTodo">
-      Add
-    </button>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+    <span class="addContainer" @click="addTodo">
+      <i class="fas fa-folder-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
@@ -26,6 +26,39 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+    display: flex;
+  }
+  .inputBox input {
+    width: 100%;
+    height: 100%;
+    border-style: none;
+    font-size: 20px;
+    padding: 0 0 0 20px;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, rgb(255, 0, 0), purple);
+    display: flex;
+    width: 50px;
+    border-radius: 0 5px 5px 0;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+  .addContainer:hover {
+    cursor: pointer;
+  }
+  .addBtn {
+    font-size: 30px;
+    color: white;
+  }
 </style>
