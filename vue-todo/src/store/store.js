588d40a28e9de0724061fb7b380b96ce5841cb45
Parkjunwoo <sinyoorang@gmail.com>
2019-06-23 00:09:56 +0900

    정리

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import * as getters from './getters.js'
-import * as mutations from './mutations.js'
 
 Vue.use(Vuex);
 
@@ -24,6 +22,30 @@ export const store = new Vuex.Store({
   state: {
     todoItems: storage.fetch() 
   },
-  getters,
-  mutations
+  getters: {
+    getTodoItems(state) {
+      return state.todoItems;
+    }
+  },
+  mutations: {
+    addOneItem(state, todoItem) {
+      console.log('worked!');
+      const obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      state.todoItems.push(obj);
+    },
+    removeOneItem(state, payload) {
+      localStorage.removeItem(payload.todoItem.item);
+      state.todoItems.splice(payload.index, 1);
+    },
+    toggleOneItem(state, payload) {
+      state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed; 
+      localStorage.removeItem(payload.todoItem.item);
+      localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+    },
+    removeAll(state) {
+      localStorage.clear();
+      state.todoItems = [];
+    }
+  }
 });
